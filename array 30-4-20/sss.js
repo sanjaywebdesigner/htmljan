f3c6b486cd146a3e6cbe03db964923f193ea8512
Sanjay Jain <sanjay21121973jain@gmail.com>
2020-04-30 17:26:52 +0530

    Add files via upload

---
+++ array 30-4-20/sss.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+function ss()
+		{
+			a=document.querySelectorAll("input");
+			a1=Number(a[0].value);
+			a2=Number(a[1].value);
+			a2=a1+a2;
+			a[2].value=a2;
+		}
+		function ss1()
+		{
+			a=document.querySelectorAll("input");
+			a1=Number(a[0].value);
+			a2=Number(a[1].value);
+			a2=a1-a2;
+			a[2].value=a2;
+		}
+		function ss2()
+		{
+			a=document.querySelectorAll("input");
+			a1=Number(a[0].value);
+			a2=Number(a[1].value);
+			a2=a1*a2;
+			a[2].value=a2;
+		}
+		function ss3()
+		{
+			a=document.querySelectorAll("input");
+			a1=Number(a[0].value);
+			a2=Number(a[1].value);
+			a2=a1/a2;
+			a[2].value=a2;
+		}
+	
---
+++ array 30-4-20/sss1.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+function gg()
+{
+	a=document.querySelectorAll("input");
+			a1=Number(a[0].value);
+			a2=Number(a[1].value);
+}
+function ss()
+		{
+			gg();
+			a2=a1+a2;
+			a[2].value=a2;
+		}
+		function ss1()
+		{
+			gg();
+			a2=a1-a2;
+			a[2].value=a2;
+		}
+		function ss2()
+		{
+			gg();
+			a2=a1*a2;
+			a[2].value=a2;
+		}
+		function ss3()
+		{
+		    gg();
+			a2=a1/a2;
+			a[2].value=a2;
+		}
+	
